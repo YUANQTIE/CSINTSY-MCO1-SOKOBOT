6d6b5259e554364edc5acbc4a575936d7105d9c9
qinn1129 <qinpei_lu@dlsu.edu.ph>
2025-09-28 17:14:15 +0800

    Create Queue.java

---
+++ Queue.java
@@ -0,0 +1,21 @@
+package solver;
+import java.util.PriorityQueue;
+public class Queue {
+    private PriorityQueue<Node> q = new PriorityQueue<>();
+
+    public void enqueue(Node node) {
+        q.add(node);
+    }
+
+    public Node dequeue() {
+        return q.poll();
+    }
+
+    public boolean isEmpty() {
+        return q.isEmpty();
+    }
+
+    public int size() {
+        return q.size();
+    }
+}
