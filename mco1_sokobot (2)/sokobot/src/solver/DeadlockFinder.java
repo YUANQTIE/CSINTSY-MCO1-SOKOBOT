fa49b55101b0b9b3cc0c2a09907e3d3647029d93
qinn1129 <qinpei_lu@dlsu.edu.ph>
2025-10-11 22:52:46 +0800

    Update DeadlockFinder.java

--- mco1_sokobot (2)/sokobot/src/solver/DeadlockFinder.java
+++ mco1_sokobot (2)/sokobot/src/solver/DeadlockFinder.java
@@ -1,3 +1,22 @@
+package solver;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+
 public class DeadlockFinder{
     //TO IMPLEMENT: Simple Deadlock Finder, Freeze Deadlock Finder. these functions should utilize the zobrist hashing to store the deadlocked states there.
+    private int[] flatMap;
+    private final int width;
+    private final int height;
+    private HashSet<Long> deadlockedStates;
+    private Zobrist zobristHash;
+    
+    public DeadlockFinder(int[] flatMap, int width, int height, Zobrist zobristHash) {
+        this.flatMap = flatMap;
+        this.width = width;
+        this.height = height;
+        this.zobristHash = zobristHash;
+        this.deadlockedStates = new HashSet<>();
+    }
 }
